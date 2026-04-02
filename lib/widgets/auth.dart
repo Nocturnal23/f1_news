1f3bf9b6f040a54af82fa808bfb597c0d3a84733
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-04-02 18:27:11 +0200

    Convertito Form in FormBuilder.

--- lib/widgets/auth.dart
+++ lib/widgets/auth.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 
 import 'homepage.dart';
 
@@ -13,24 +15,25 @@ class _AuthState extends State<Auth> {
   String? username;
   String? email;
   String? password;
-  final _formKey = GlobalKey<FormState>(); // Questa chiave serve per verificare la validità del form.
+  final _formKey = GlobalKey<FormBuilderState>(); // Questa chiave serve per verificare la validità del form.
   Map signUp = {"username":"", "email":"", "password":""};
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
+      body: FormBuilder(
         key: _formKey,
         child: Column(
           mainAxisAlignment: .center,
           children: [
-            TextFormField(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return "Devi inserire lo userneame!";
-                }
-                return null;
-              },
+            FormBuilderTextField(
+              name: 'username',
+              // validator: (value) {
+              //   if (value == null || value.isEmpty) {
+              //     return "Devi inserire lo userneame!";
+              //   }
+              //   return null;
+              // },
               onSaved: (value){
                 signUp["username"] = value;
               },
@@ -40,33 +43,41 @@ class _AuthState extends State<Auth> {
                 icon: Icon(Icons.person),
                 labelText: 'Username',
               ),
+              validator: FormBuilderValidators.compose([
+                FormBuilderValidators.required(),
+              ]),
             ),
 
-            TextFormField(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return "Devi inserire una mail valida!";
-                }
-                return null;
-              },
+            FormBuilderTextField(
+              name: 'email',
+              // validator: (value) {
+              //   if (value == null || value.isEmpty) {
+              //     return "Devi inserire una mail valida!";
+              //   }
+              //   return null;
+              // },
               onSaved: (value){
                 signUp["email"] = value;
               },
-              textInputAction: TextInputAction.next,
-              //Con invio passo al campo successivo.
+              textInputAction: TextInputAction.next, //Con invio passo al campo successivo.
               decoration: const InputDecoration(
                 icon: Icon(Icons.mail),
                 labelText: 'Email',
               ),
+              validator: FormBuilderValidators.compose([
+                FormBuilderValidators.required(),
+                FormBuilderValidators.email(),
+              ]),
             ),
 
-            TextFormField(
-              validator: (value) {
-                if (value == null || value.isEmpty || value.length < 10) {
-                  return "Devi inserire una password valida!";
-                }
-                return null;
-              },
+            FormBuilderTextField(
+              name: 'password',
+              // validator: (value) {
+              //   if (value == null || value.isEmpty || value.length < 10) {
+              //     return "Devi inserire una password valida!";
+              //   }
+              //   return null;
+              // },
               onSaved: (value){
                 signUp["password"] = value;
               },
@@ -75,12 +86,15 @@ class _AuthState extends State<Auth> {
                 icon: Icon(Icons.password),
                 labelText: 'Password',
               ),
+              validator: FormBuilderValidators.compose([
+                FormBuilderValidators.required(),
+              ]),
             ),
 
             ElevatedButton(
               onPressed: () {
                 if (_formKey.currentState!.validate()) {
-                  _formKey.currentState!.save(); // Se il form risulta valido salvo le credenziali.
+                  _formKey.currentState!.saveAndValidate(); // Se il form risulta valido salvo le credenziali.
                   print(signUp);
                 }
               },
