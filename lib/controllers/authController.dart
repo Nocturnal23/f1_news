c490fb33ec65830a4430a723c5b5aea1adc0350a
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-04-09 00:07:47 +0200

    Aggiunta funzione per la verifica della mail.

--- lib/controllers/authController.dart
+++ lib/controllers/authController.dart
@@ -22,17 +22,56 @@ class AuthController {
   Future<void> signUp({required String user, required String email, required String password}) async {
     try {
       await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+      await sendVerificationEmail();
+
     } on FirebaseAuthException catch(e) {
       print('Failed with error code: ${e.code}');
       print(e.message);
       rethrow;
     }
-
   }
-
 
   //Funzione pr il logout.
   Future<void> signOut() async {
-    await _firebaseAuth.signOut();
+    try {
+      await _firebaseAuth.signOut();
+    } on FirebaseAuthException catch(e) {
+      print('Failed with error code: ${e.code}');
+      print(e.message);
+      rethrow;
+    }
+  }
+
+  //Gunzione per l'invio della mail di verifica.
+  Future<void> sendVerificationEmail() async {
+    try {
+      await _firebaseAuth.currentUser?.sendEmailVerification();
+    } on FirebaseAuthException catch (e) {
+      print('Failed with error code: ${e.code}');
+      print(e.message);
+      rethrow;
+    }
+  }
+
+  //Funzione per la verifica della mail.
+  Future<bool> isEmailVerified() async {
+    try {
+      await _firebaseAuth.currentUser?.reload();
+      return _firebaseAuth.currentUser?.emailVerified ?? false;
+    } on FirebaseAuthException catch (e) {
+      print('Failed with error code: ${e.code}');
+      print(e.message);
+
+      return false;
+    }
   }
 }
+// Salvare lo username dell'utente.
+// Conferma account via mail. OK.
+// Recupero password.
+// Accedi via google.
+
+/* Nota: Costruire una classe dizionario per contenere gli errori.
+Magari costruire un enum dove vengono associati gli errori cosi da usare questa
+classe in giro per il codice anzichè inserire a mano le stringhe.
+ */
