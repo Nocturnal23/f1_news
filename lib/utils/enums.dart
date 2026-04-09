4d9aa7776ddf5c1d1b5ef1e093cdfd5161ebbb7b
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-04-09 14:18:47 +0200

    Enum aggiornato.

--- lib/utils/enums.dart
+++ lib/utils/enums.dart
@@ -1,6 +1,7 @@
 enum ErrorsEnums {
   INVALID_CREDENTIAL('invalid-credential'),
   EMAIL_ALREADY_IN_USE('email-already-in-use'),
+  INVALID_EMAIL('invalid-email'),
   UNKNOWN('unknown');
 
   final String label;
