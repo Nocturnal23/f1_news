7b201831a11e5245264b02c283091af20d132f40
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-04-12 18:52:58 +0200

    Creato un widget AppBar da riutilizzare nel codice.

---
+++ lib/widgets/app_bar_custom.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+import '../controllers/auth_controller.dart';
+
+enum MenuOptions { impostazioniAccount, impostazioniApp, preferiti, logout }
+
+class AppBarCustom extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final bool isAnonymous;
+  final VoidCallback onSignOut;
+  final user = AuthController().currentUser;
+
+  AppBarCustom({
+    super.key,
+    required this.title,
+    required this.isAnonymous,
+    required this.onSignOut,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.red,
+      leadingWidth: 100,
+
+      leading: isAnonymous
+          ? Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 3.0),
+              child: ElevatedButton(
+                onPressed: onSignOut,
+                child: const Text("Accedi"),
+              ),
+            )
+          : null,
+
+      centerTitle: true,
+      title: Text(title),
+
+      actions: [
+        if (!isAnonymous)
+          PopupMenuButton<MenuOptions>(
+            icon: const Icon(Icons.account_circle_outlined),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            position: PopupMenuPosition.under,
+            onSelected: (MenuOptions value) {
+              switch (value) {
+                case MenuOptions.impostazioniAccount:
+                  print("Vai al profilo");
+                  break;
+                case MenuOptions.impostazioniApp:
+                  print("Apri impostazioni");
+                  break;
+                case MenuOptions.preferiti:
+                  print("Gestisci i tuoi preferiti");
+                  break;
+                case MenuOptions.logout:
+                  onSignOut();
+                  break;
+              }
+            },
+            itemBuilder: (BuildContext context) =>
+                <PopupMenuEntry<MenuOptions>>[
+                  const PopupMenuItem<MenuOptions>(
+                    value: MenuOptions.impostazioniAccount,
+                    child: ListTile(
+                      leading: Icon(Icons.person),
+                      title: Text('Profilo'),
+                    ),
+                  ),
+                  const PopupMenuItem<MenuOptions>(
+                    value: MenuOptions.impostazioniApp,
+                    child: ListTile(
+                      leading: Icon(Icons.settings),
+                      title: Text('Impostazioni'),
+                    ),
+                  ),
+                  const PopupMenuItem<MenuOptions>(
+                    value: MenuOptions.preferiti,
+                    child: ListTile(
+                      leading: Icon(Icons.favorite),
+                      title: Text('Preferiti'),
+                    ),
+                  ),
+                  const PopupMenuItem<MenuOptions>(
+                    value: MenuOptions.logout,
+                    child: ListTile(
+                      leading: Icon(Icons.exit_to_app, color: Colors.red),
+                      title: Text('Esci', style: TextStyle(color: Colors.red)),
+                    ),
+                  ),
+                ],
+          ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
