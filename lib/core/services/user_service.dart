5ebdc3adbf29b5bc919cf04233a5841042a5a0ed
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-04-12 18:49:00 +0200

    Aggiunto UserService per recuperare i dati utente da Firestore come stream, con mapping a UserModel.

---
+++ lib/core/services/user_service.dart
@@ -0,0 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/user_models.dart';
+
+class UserService {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  Stream<UserModel?> getUser(String uid) {
+    return _db.collection('users').doc(uid).snapshots().map((snapshot) {
+      if (snapshot.exists) {
+        return UserModel.fromMap(snapshot.data(), snapshot.id);
+      }
+      return null;
+    });
+  }
+}
