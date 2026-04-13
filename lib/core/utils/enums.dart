5f24ed066d203c344667b10ae6b4c20418582f2e
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-04-13 23:56:58 +0200

    Nuove voci per gestire gli errori.

--- lib/core/utils/enums.dart
+++ lib/core/utils/enums.dart
@@ -2,6 +2,8 @@ enum ErrorsEnums {
   INVALID_CREDENTIAL('invalid-credential'),
   EMAIL_ALREADY_IN_USE('email-already-in-use'),
   INVALID_EMAIL('invalid-email'),
+  EMAIL_NOT_VERIFIED('email-not-verified'),
+  GOOGLE_SIGNIN_ABORTED('google-sign-in-aborted-by-user'),
   UNKNOWN('unknown');
 
   final String label;
