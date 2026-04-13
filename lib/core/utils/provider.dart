339f7f7209a405d2abfe58a807d134d3b5d0ed85
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-04-13 23:58:02 +0200

    Lo stream di firebase è ora nel provvider.

--- lib/core/utils/provider.dart
+++ lib/core/utils/provider.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/user_models.dart';
 import '../services/user_service.dart';
 
 final userStreamProvider = StreamProvider.family<UserModel?, String>((ref, uid) {
   return UserService().getUser(uid);
+});
+
+final authStateProvider = StreamProvider<User?>((ref) {
+  return FirebaseAuth.instance.authStateChanges();
 });
