ec7cde1fa2a360b1a3e712f6c31db176596c97fb
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-04-16 16:51:12 +0200

    Differenziati le repo per gestire il recupero dei piloti.

--- lib/core/repository/jolpica_repository.dart
+++ lib/core/repository/jolpica_repository.dart
@@ -1,3 +1,4 @@
+import '../models/drivers_models_standings.dart';
 import '../models/drivers_models.dart';
 import '../services/jolpica_service.dart';
 
@@ -6,16 +7,35 @@ class F1Repository {
 
   F1Repository(this.apiService);
 
-  // Future<List<DriverModel>> fetchDrivers() async {
-  //   final data = await apiService.getDrivers();
-  //
-  //   final driversJson = data['MRData']['DriverTable']['Drivers'];
-  //
-  //   return driversJson
-  //       .map<DriverModel>((json) => DriverModel.fromJson(json))
-  //       .toList();
-  // }
+  //Repo per i piloti che sono in classifica.
+  Future<List<DriverModelStandings>> fetchDriversStandings() async {
+    try {
+      final data = await apiService.getDriversStandings();
 
+      if (data['MRData'] != null &&
+          data['MRData']['StandingsTable'] != null &&
+          data['MRData']['StandingsTable']['StandingsLists'] != null) {
+
+        final List<dynamic> standingsLists = data['MRData']['StandingsTable']['StandingsLists'];
+
+        if (standingsLists.isNotEmpty) {
+          final List<dynamic>? standingsJson = standingsLists[0]['DriverStandings'];
+
+          if (standingsJson != null) {
+            return standingsJson
+                .map((json) => DriverModelStandings.fromJson(json))
+                .toList();
+          }
+        }
+      }
+      return [];
+    } catch (e) {
+      print("Errore nel repository: $e");
+      rethrow;
+    }
+  }
+
+  //Repo per tutti i piloti che hanno preso parte ad almeno una sessione ufficiela.
   Future<List<DriverModel>> fetchDrivers() async {
     try {
       final data = await apiService.getDrivers();
@@ -24,14 +44,15 @@ class F1Repository {
           data['MRData']['DriverTable'] != null &&
           data['MRData']['DriverTable']['Drivers'] != null) {
 
-        final List<dynamic> driversJson = data['MRData']['DriverTable']['Drivers'];
+        final List<dynamic> driversJson =
+        data['MRData']['DriverTable']['Drivers'];
 
         return driversJson
             .map((json) => DriverModel.fromJson(json))
             .toList();
-      } else {
-        return [];
       }
+
+      return [];
     } catch (e) {
       print("Errore nel repository: $e");
       rethrow;
