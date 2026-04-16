4f1ff92df089991ea5847fbe7975e131ec25373f
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-04-16 16:50:52 +0200

    Differenziati gli endpoint. getDriversStandings recupera i piloti presenti in classifica. getDrives recupera tutti i piloti.

--- lib/core/services/jolpica_service.dart
+++ lib/core/services/jolpica_service.dart
@@ -4,13 +4,35 @@ import 'package:http/http.dart' as http;
 class ApiService {
   static const String baseUrl = 'https://api.jolpi.ca/ergast/f1';
 
-  Future<Map<String, dynamic>> getDrivers() async {
-    final response = await http.get(Uri.parse('$baseUrl/${DateTime.now().year}/drivers.json'));
+  //Endpoint per i piloti presenti in classifica.
+  Future<Map<String, dynamic>> getDriversStandings() async {
+    final response = await http.get(Uri.parse('$baseUrl/${DateTime.now().year}/driverStandings.json'));
 
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
       throw Exception('Errore nel caricamento dei piloti: ${response.statusCode}');
+    }
+  }
+
+  //Endpoint per tutti i piloti che hanno preso parte ad almeno una sessione ufficiale.
+  Future<Map<String, dynamic>> getDrivers() async {
+    final response = await http.get(Uri.parse('$baseUrl/${DateTime.now().year}/drivers.json?limit=100'));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Errore nel caricamento dei piloti: ${response.statusCode}');
+    }
+  }
+
+  Future<Map<String, dynamic>> getTeams() async {
+    final response = await http.get(Uri.parse('$baseUrl/${DateTime.now().year}/constructors.json'));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Errore nel caricamento dei team: ${response.statusCode}');
     }
   }
 }
