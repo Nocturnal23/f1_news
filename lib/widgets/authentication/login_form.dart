e70ec72fdcf7b4032c7a8dd115d200d4cee792a7
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-04-17 17:08:21 +0200

    Spostati widget in \authentication. Sono widget usati nella fase di autenticazione.

--- lib/widgets/authentication/login_form.dart
+++ lib/widgets/authentication/login_form.dart
@@ -5,8 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
-import '../controllers/auth_controller.dart';
-import '../core/utils/provider.dart';
+import '../../controllers/auth_controller.dart';
+import '../../core/utils/provider.dart';
 
 class LoginForm extends ConsumerStatefulWidget {
   const LoginForm({super.key});
--- lib/widgets/authentication/register_form.dart
+++ lib/widgets/authentication/register_form.dart
@@ -5,8 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
-import '../core/utils/provider.dart';
-import 'info_dialog_alert.dart';
+import '../../core/utils/provider.dart';
+import '../info_dialog_alert.dart';
 
 class RegisterForm extends ConsumerStatefulWidget {
   const RegisterForm({super.key});
