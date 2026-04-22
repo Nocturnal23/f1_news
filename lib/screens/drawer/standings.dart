b5f97469f9160952076e20e58247252ddb1df07c
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-04-22 16:34:26 +0200

    Aggiunto standings.dart che rappresenta il contenitore per le due classifiche.

---
+++ lib/screens/drawer/standings.dart
@@ -0,0 +1,39 @@
+import 'package:f1_news/widgets/navigation/app_bar_custom.dart';
+import 'package:f1_news/widgets/navigation/drawer_app.dart';
+import '../../widgets/standings/constructorsStandings.dart';
+import '../../widgets/standings/driversStandings.dart';
+import 'package:flutter/material.dart';
+
+import '../../widgets/standings/standingsList.dart';
+
+class Standings extends StatelessWidget {
+  const Standings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBarCustom(
+          title: "Classifica",
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: "Piloti"),
+              Tab(text: "Costruttori"),
+            ],
+          ),
+        ),
+      
+        drawer: const DrawerApp(),
+
+        body: TabBarView(
+          children: [
+            StandingsList(type: "drivers"),
+            StandingsList(type: "constructors"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
