03863ce911003e5fa34a8b8c7a6fed740b35ec5e
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-04-24 22:22:00 +0200

    Widget Card per mostrare elementi quali le gare.

---
+++ lib/widgets/card_custom.dart
@@ -0,0 +1,80 @@
+import 'package:country_flags/country_flags.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../core/utils/country_helper.dart';
+
+class CardCustom extends StatelessWidget {
+  dynamic item;
+
+  CardCustom({super.key, this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    final String isoCode = CountryHelper.getIsoCode(item.country);
+
+    return SizedBox(
+      height: 250,
+      width: double.infinity,
+      child: Card(
+        clipBehavior: Clip.hardEdge,
+        elevation: 4,
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Opacity(
+                opacity: 0.6,
+                child: isoCode.isNotEmpty
+                    ? CountryFlag.fromCountryCode(isoCode, theme: ImageTheme())
+                    : Container(color: Colors.grey),
+              ),
+            ),
+
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.black.withValues(alpha: 0.8), Colors.transparent],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          item.raceName ?? "Grand Prix",
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "${item.fp1Date} - ${item.date}",
+                          style: const TextStyle(
+                            color: Colors.white70,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Icon(Icons.map, size: 80, color: Colors.white),
+                  //Image.asset(assetsUrl)
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
