870d540ad3637c93d7ffce6e0eae7a113b2d6b1a
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-04-29 15:23:36 +0200

    multiendpoint-info: RaceDetailsModel è il modello per i dati, firstEdition e lastWinner verranno dalle Jolpica API. trackLenght e lapRecord da vedere.

---
+++ lib/core/models/race_details_model.dart
@@ -0,0 +1,13 @@
+class RaceDetailsModel {
+  final String firstEdition;
+  final String lastWinner;
+  // final String trackLength;
+  // final String lapRecord;
+
+  RaceDetailsModel({
+    required this.firstEdition,
+    required this.lastWinner,
+    // required this.trackLength,
+    // required this.lapRecord,
+  });
+}
