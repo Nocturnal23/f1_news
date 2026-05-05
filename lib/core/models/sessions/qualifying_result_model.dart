aaf9219848840b01c9911b41491852d6ce3f17f2
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-05-05 17:50:59 +0200

    Aggiunto model per le qualifiche.

---
+++ lib/core/models/sessions/qualifying_result_model.dart
@@ -0,0 +1,39 @@
+import 'package:f1_news/core/models/sessions/base_result_model.dart';
+
+import '../constructor_model.dart';
+import '../driver_model.dart';
+
+class QualifyingResultModel extends BaseResultModel {
+  final String q1;
+  final String q2;
+  final String q3;
+
+  QualifyingResultModel({
+    required super.position,
+    required super.number,
+    required super.driver,
+    required super.constructor,
+    required this.q1,
+    this.q2 = "",
+    this.q3 = "",
+  });
+
+  factory QualifyingResultModel.fromJson(Map<String, dynamic> json) {
+    return QualifyingResultModel(
+      position: json['position'] ?? 'N/A',
+      number: json['number'] ?? 'N/A',
+      driver: DriverModel.fromJson(json['Driver']),
+      constructor: ConstructorModel.fromJson(json['Constructor']),
+
+      q1: json['Q1'] ?? '',
+      q2: json['Q2'] ?? '',
+      q3: json['Q3'] ?? '',
+    );
+  }
+
+  @override
+  String get displayPosition => position;
+
+  @override
+  List<String> get extraColumns => [q1, q2, q3];
+}
