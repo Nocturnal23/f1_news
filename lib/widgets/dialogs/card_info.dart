57d98b3b7ab6f67d4d67e4d2b049a2898af76999
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-05-08 17:20:03 +0200

    Refactor: card_info.dart è stata adattata al responsive.

--- lib/widgets/dialogs/card_info.dart
+++ lib/widgets/dialogs/card_info.dart
@@ -1,57 +1,65 @@
 import 'package:f1_news/core/models/race_details.dart';
+import 'package:f1_news/core/providers/screenProvider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CardInfo extends StatelessWidget {
+class CardInfo extends ConsumerWidget {
   final String img;
   final RaceDetailsModel details;
 
   CardInfo({super.key, required this.img, required this.details});
 
   @override
-  Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    final isSmallScreen = screenWidth < 360;
+  Widget build(BuildContext context, WidgetRef sRef) {
+    final sizeScreen = sRef.watch(screenProvider);
 
     return Dialog(
       insetPadding: const EdgeInsets.all(16),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       child: ConstrainedBox(
-        constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height * 0.8),
+        constraints: BoxConstraints(maxHeight: sizeScreen.height * 0.8),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(img, fit: BoxFit.contain),
             Padding(
-              padding: EdgeInsets.all(isSmallScreen ? 12.0 : 16.0),
-        
+              padding: EdgeInsets.all(sizeScreen.isSmallPhone ? 12.0 : 16.0),
+
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  _buildData(
-                    "Lunghezza",
-                    "${details.trackLength} km",
-                    isSmallScreen,
+                  GridView.count(
+                    crossAxisCount: 2,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    childAspectRatio: sizeScreen.isSmallPhone ? 1.5 : 1.8,
+
+                    children: [
+                      _buildData("Prima edizione", "${details.firstEdition}", sizeScreen.isSmallPhone),
+                      _buildData("Ultimo vincitore", "${details.lastWinner}", sizeScreen.isSmallPhone),
+                    ],
                   ),
-        
-                  SizedBox(height: isSmallScreen ? 12 : 20),
-        
+
+
+                  SizedBox(height: sizeScreen.isSmallPhone ? 12 : 20),
+
                   LayoutBuilder(
                       builder: (context, constraints) {
                         return GridView.count(
                           crossAxisCount: 2,
                           shrinkWrap: true,
                           physics: const NeverScrollableScrollPhysics(),
-                          childAspectRatio: isSmallScreen ? 1.5 : 1.8,
+                          childAspectRatio: sizeScreen.isSmallPhone ? 1.5 : 1.8,
                           mainAxisSpacing: 10,
                           crossAxisSpacing: 10,
                           children: [
-                            _buildData("Prima edizione", "${details.firstEdition}", isSmallScreen),
+                            _buildData("Lunghezza", "${details.trackLength} km", sizeScreen.isSmallPhone),
 
-                            _buildData("Giro veloce", "${details.lapRecord}", isSmallScreen),
+                            _buildData("Giro veloce", "${details.lapRecord}", sizeScreen.isSmallPhone),
 
-                            _buildData("Numero di giri", "${details.lapsNumber}", isSmallScreen),
+                            _buildData("Numero di giri", "${details.lapsNumber}", sizeScreen.isSmallPhone),
 
-                            _buildData("Distanza gara", "${(details.trackLength * details.lapsNumber).toStringAsFixed(3)} km", isSmallScreen),
+                            _buildData("Distanza gara", "${(details.trackLength * details.lapsNumber).toStringAsFixed(3)} km", sizeScreen.isSmallPhone),
                           ],
                         );
                       },
