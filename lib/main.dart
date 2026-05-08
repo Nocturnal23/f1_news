1db7c5174680c2deb222bedae8d2ccecd12ca2a1
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-05-08 17:19:13 +0200

    Inizializzazione nel main della funzione per determinare la dimensione dello schermo.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/navigation/routes.dart';
+import 'core/providers/screenProvider.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -23,28 +24,44 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Homepage(),
+  Widget build(BuildContext context, WidgetRef sRef) {
+    final Size size = MediaQuery.of(context).size;
 
-      routes: {
-        Routes.homepage: (context) => const Homepage(),
-        Routes.auth: (context) => const Auth(),
-        Routes.drivers: (context) => const Drivers(),
-        Routes.teams: (context) => const Constructors(),
-        Routes.standings: (context) => const Standings(),
-        Routes.races: (context) => Races(),
-        // Routes.profile: (context) => const Profile(),
-        // Routes.settings: (context) => const Settings(),
-        // Routes.favorite: (context) => const Favorite(),
-        // Routes.news: (context) => const News(),
+    return ProviderScope(
+      overrides: [
+        screenProvider.overrideWithValue(
+          ScreenProvider(
+            width: size.width,
+            height: size.height,
+            isSmallPhone: size.width < 360,
+            isStandardPhone: size.width >= 360 && size.width < 600,
+            isTablet: size.width >= 600,
+          ),
+        ),
+      ],
 
-      },
+      child: MaterialApp(
+        home: Homepage(),
+
+        routes: {
+          Routes.homepage: (context) => const Homepage(),
+          Routes.auth: (context) => const Auth(),
+          Routes.drivers: (context) => const Drivers(),
+          Routes.teams: (context) => const Constructors(),
+          Routes.standings: (context) => const Standings(),
+          Routes.races: (context) => Races(),
+          // Routes.profile: (context) => const Profile(),
+          // Routes.settings: (context) => const Settings(),
+          // Routes.favorite: (context) => const Favorite(),
+          // Routes.news: (context) => const News(),
+
+        },
+      ),
     );
   }
 }
