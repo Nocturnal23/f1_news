9775233fffe7a384becd382cfb8ad4a8be038fd9
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-05-20 18:02:46 +0200

    Creata la schermata NewsList per mostrare la lista di notizie.
    Aggiunto il widget NewsCard per mostrare immagine, titolo, descrizione e data di ogni notizia.

---
+++ lib/screens/drawer/news_list.dart
@@ -0,0 +1,65 @@
+import 'package:f1_news/widgets/navigation/app_bar_custom.dart';
+import 'package:f1_news/widgets/navigation/drawer_app.dart';
+import 'package:flutter/material.dart';
+
+import '../../core/models/article.dart';
+import '../../core/services/rss_service.dart';
+import '../../widgets/racing/news_card.dart';
+
+class NewsList extends StatefulWidget {
+  const NewsList({super.key});
+
+  @override
+  State<NewsList> createState() => _NewsListState();
+}
+
+class _NewsListState extends State<NewsList> {
+  final RssService _rssService = RssService();
+  late Future<List<Article>> _futureArticles;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureArticles = _rssService.fetchAllNews();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarCustom(title: "Ultime notizie"),
+
+      drawer: DrawerApp(),
+
+      body: FutureBuilder<List<Article>>(
+        future: _futureArticles,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Errore: ${snapshot.error}'));
+          }
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('Nessuna notizia trovata.'));
+          }
+
+          final articles = snapshot.data!;
+
+          return ListView.builder(
+            itemCount: articles.length,
+            itemBuilder: (context, index) {
+              final article = articles[index];
+              return NewsCard(
+                title: article.title,
+                description: article.description,
+                pubDate: article.pubDate,
+                link: article.link,
+                imageUrl: article.imageUrl,
+              );
+            },
+          );
+        }
+      ),
+    );
+  }
+}
---
+++ lib/widgets/racing/news_card.dart
@@ -0,0 +1,79 @@
+import 'package:f1_news/widgets/dialogs/info_dialog_alert.dart';
+import 'package:flutter/material.dart';
+
+import '../article_page.dart';
+
+class NewsCard extends StatelessWidget {
+  final String title;
+  final String description;
+  final String pubDate;
+  final String link;
+  final String imageUrl;
+
+  const NewsCard({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.pubDate,
+    required this.link,
+    this.imageUrl = ''
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Card(
+        elevation: 2,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            imageUrl.isNotEmpty
+                ? Image.network(imageUrl)
+                : SizedBox.shrink(),
+
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    description,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    pubDate,
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => ArticlePage(
+              title: title,
+              url: link,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
