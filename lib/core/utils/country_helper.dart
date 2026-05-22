a5b6181e45d8c22e57f2aa43762cfa496dd7f87c
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-05-22 17:09:32 +0200

    Aggiunta funzione per la traduzione delle nazionalita in nome del relativo paese.

--- lib/core/utils/country_helper.dart
+++ lib/core/utils/country_helper.dart
@@ -7,11 +7,53 @@ class CountryHelper {
     "UAE": "United Arab Emirates",
   };
 
+  static const Map<String, String> _nationalityToIso = {
+    'British': 'gb',
+    'German': 'de',
+    'Spanish': 'es',
+    'Finnish': 'fi',
+    'Japanese': 'jp',
+    'French': 'FR',
+    'Mexican': 'mx',
+    'Monegasque': 'mc',
+    'Australian': 'au',
+    'Austrian': 'at',
+    'Dutch': 'nl',
+    'Canadian': 'ca',
+    'Thai': 'th',
+    'Chinese': 'cn',
+    'American': 'us',
+    'Italian': 'it',
+    'Swiss': 'ch',
+    'New zealander': 'nz',
+    'Argentine': 'ar',
+    'Brazilian': 'br',
+    'Danish': 'dk',
+    'Belgian': 'be',
+    'Colombian': 'co',
+    'Polish': 'pl',
+    'Russian': 'ru',
+    'Swedish': 'se',
+    'Venezuelan': 've',
+    'Indian': 'in',
+    'Indonesian': 'id',
+  };
+
   static String getIsoCode(String? countryName) {
     String? cleanName = countryAlias[countryName] ?? countryName;
-
     try {
       final country = Countries.byName(cleanName!);
+      return country?.alpha2Code ?? "";
+    } catch (e) {
+      return "";
+    }
+  }
+
+  static String getIsoCodeFromNationality(String nationality) {
+    if (nationality.isEmpty) return '';
+    String code = _nationalityToIso[nationality]?.toUpperCase() ?? '';
+    try {
+      final country = Countries.byCode(code);
       return country?.alpha2Code ?? "";
     } catch (e) {
       return "";
