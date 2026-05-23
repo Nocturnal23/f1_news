852b7160df4cdffb756aa68688f7008c3208973f
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-05-23 15:33:06 +0200

    competitors_list.dart aggiunta possibilita di aprire scheda dati pilota/costruttore.

--- lib/screens/drawer/competitors_list.dart
+++ lib/screens/drawer/competitors_list.dart
@@ -116,10 +116,12 @@ class CompetitorsList extends ConsumerWidget {
     final isFav = favoriteIds.contains(id);
 
     return InkWell(
-      onTap: () {
+      onTap: () async {
+        final repository = ref.read(f1RepositoryProvider);
+        final extraData = await repository.fetchCompetitorExtra(type, id);
         showDialog(
           context: context,
-          builder: (context) => CardCompetitor(type: type, item: itemData, name: title),
+          builder: (context) => CardCompetitor(type: type, item: itemData, name: title, extraData: extraData),
         );
       },
 
