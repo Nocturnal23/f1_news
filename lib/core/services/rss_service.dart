9450aeae4ebf9cf584a8a2a5cc182cf3161e14dd
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-05-24 20:24:43 +0200

    Separazione responsabilità rss_service.dart si occupa di contattare gli rss e restituire il response al repository.

--- lib/core/services/rss_service.dart
+++ lib/core/services/rss_service.dart
@@ -1,35 +1,20 @@
 import 'dart:async';
 
 import 'package:http/http.dart' as http;
-import 'package:rss_dart/dart_rss.dart';
-
-import '../common/rss_list.dart';
-import '../models/article.dart';
 
 class RssService {
-  Future<List<Article>> fetchAllNews() async {
-    List<Article> allArticles = [];
+  Future<String> fetchRawXml(String url) async {
+    final response = await http.get(Uri.parse(url)).timeout(
+      const Duration(seconds: 10),
+      onTimeout: () {
+        throw TimeoutException('Il server ha impiegato troppo tempo per rispondere.');
+      },
+    );
 
-    for (var url in RssList.feedUrls.values) {
-      try {
-        final response = await http.get(Uri.parse(url)).timeout(const Duration(seconds: 10),
-            onTimeout: () {
-              throw TimeoutException('Il server ha impiegato troppo tempo per rispondere. Riprova.');
-            });
-
-        if (response.statusCode == 200) {
-          final feed = RssFeed.parse(response.body);
-          for (var item in feed.items) {
-            allArticles.add(Article.fromRssItem(item));
-          }
-        } else {
-          throw Exception('Server error: ${response.statusCode}');
-        }
-      } catch (e) {
-        throw Exception('Errore durante il fetch di $url: $e');
-      }
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw Exception('Errore durante la connessione: ${response.statusCode}');
     }
-
-    return allArticles;
   }
 }
