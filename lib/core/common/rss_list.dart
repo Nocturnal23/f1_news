32c9feb9690bab58ba5886f2f783446aa328a593
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-05-24 20:27:36 +0200

    Aggiunte fonti.

--- lib/core/common/rss_list.dart
+++ lib/core/common/rss_list.dart
@@ -2,6 +2,9 @@ class RssList {
   static const Map<String, String> feedUrls = {
     'motorsport': 'https://it.motorsport.com/rss/f1/news/',
     'automoto': 'https://www.automoto.it/rss/formula1.xml',
-    'gazetta': 'https://www.gazzetta.it/dynamic-feed/rss/section/Formula-1.xml'
+    'gazetta': 'https://www.gazzetta.it/dynamic-feed/rss/section/Formula-1.xml',
+    'formulapassion': 'https://www.formulapassion.it/motorsport/formula-1/feed',
+    // 'motorionline': 'https://f1grandprix.motorionline.com/feed/'
+    // 'corriere': 'https://www.corrieredellosport.it/rss/formula-1',
   };
 }
