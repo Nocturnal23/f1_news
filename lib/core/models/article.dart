8627b4db4ce0773dd027621842b65f316188bec5
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-05-24 20:27:00 +0200

    pubDate ora è un dateTimee. _normalizzeDate si occupa di rendere tutti i formati di date uguali prima di procedere al salvataggio dell'informazione.

--- lib/core/models/article.dart
+++ lib/core/models/article.dart
@@ -1,16 +1,17 @@
+import 'package:intl/intl.dart';
 import 'package:rss_dart/dart_rss.dart';
 
 class Article {
   final String title;
   final String description;
-  final String pubDate;
+  final DateTime? pubDate;
   final String link;
   final String imageUrl;
 
   Article({
     required this.title,
     required this.description,
-    required this.pubDate,
+    this.pubDate,
     required this.link,
     this.imageUrl = '',
   });
@@ -19,9 +20,26 @@ class Article {
     return Article(
       title: item.title ?? 'Senza Titolo',
       description: item.description ?? 'Nessuna descrizione disponibile',
-      pubDate: item.pubDate?.toString() ?? '',
+      pubDate: _normalizzeDate(item.pubDate),
       link: item.link?.toString() ?? '',
       imageUrl: item.enclosure?.url ?? '',
     );
+  }
+
+  static DateTime? _normalizzeDate(String? date) {
+    if (date == null || date.trim().isEmpty) return null;
+
+    DateTime? parsedDate = DateTime.tryParse(date);
+    if (parsedDate != null) {
+      return parsedDate;
+    }
+
+    try {
+      final format = DateFormat("EEE, dd MMM yyyy HH:mm:ss Z", "en_US");
+      return format.parse(date);
+    } catch (e) {
+      print("Impossibile normalizzare la data: $date - Errore: $e");
+      return null;
+    }
   }
 }
