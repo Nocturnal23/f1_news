f1d9ae7a09995a1a59636383385c75e267a421f2
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-05-28 15:15:49 +0200

    Aggiunti RSS in inglese.

--- lib/core/common/rss_list.dart
+++ lib/core/common/rss_list.dart
@@ -4,7 +4,14 @@ class RssList {
     'automoto': 'https://www.automoto.it/rss/formula1.xml',
     'gazetta': 'https://www.gazzetta.it/dynamic-feed/rss/section/Formula-1.xml',
     'formulapassion': 'https://www.formulapassion.it/motorsport/formula-1/feed',
-    // 'motorionline': 'https://f1grandprix.motorionline.com/feed/'
-    // 'corriere': 'https://www.corrieredellosport.it/rss/formula-1',
+    'motorionline': 'https://f1grandprix.motorionline.com/feed/',
+    'corriere': 'https://www.corrieredellosport.it/rss/formula-1'
+  };
+
+  static const Map<String, String> feedUrlsEN = {
+    'therace' : 'https ://www.the-race.com/category/formula-1/rss/',
+    'motorsport_global': 'https://www.motorsport.com/rss/f1/news/',
+    'espn' : 'https://www.espn.com/espn/rss/f1/news',
+    'bbc' : 'https://feeds.bbci.co.uk/sport/formula1/rss.xml'
   };
 }
