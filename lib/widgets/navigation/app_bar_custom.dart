d8da747ef8d94cb9108a318c012399bed37df961
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-06-11 20:25:15 +0200

    refactor: app_bar_custom.dart impostazione per nascondere il bottone d'accesso nelle pagine non interessate.

--- lib/widgets/navigation/app_bar_custom.dart
+++ lib/widgets/navigation/app_bar_custom.dart
@@ -14,11 +14,13 @@ enum MenuOptions { impostazioniAccount, impostazioniApp, logout }
 class AppBarCustom extends ConsumerWidget implements PreferredSizeWidget {
   final String title;
   final PreferredSizeWidget? bottom;
+  final bool showAuthButton;
 
   const AppBarCustom({
     super.key,
     required this.title,
     this.bottom,
+    this.showAuthButton = true,
   });
 
   @override
@@ -51,67 +53,69 @@ class AppBarCustom extends ConsumerWidget implements PreferredSizeWidget {
           ),
         ),
 
-        if(user == null)
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 3.0),
-            child: SizedBox(
-              height: screen.isSmallPhone ? 35 : 40,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, Routes.auth);
-                },
-                child: Text("Accedi", style: TextStyle(fontSize: screen.isSmallPhone ? 15 : 18)),
+        if(showAuthButton) ...[
+          if(user == null)
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 3.0),
+              child: SizedBox(
+                height: screen.isSmallPhone ? 35 : 40,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, Routes.auth);
+                  },
+                  child: Text("Accedi", style: TextStyle(fontSize: screen.isSmallPhone ? 15 : 18)),
+                ),
               ),
+            )
+          else
+            PopupMenuButton<MenuOptions>(
+              icon: const Icon(Icons.account_circle_outlined),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              position: PopupMenuPosition.under,
+              onSelected: (MenuOptions value) {
+                switch (value) {
+                  case MenuOptions.impostazioniAccount:
+                    final String? currentRoute = ModalRoute.of(context)?.settings.name;
+                    if (currentRoute != Routes.profile) {
+                      Navigator.pushNamed(context, Routes.profile);
+                    }
+                    break;
+                  case MenuOptions.impostazioniApp:
+                    _showAlert(context);
+                    break;
+                  case MenuOptions.logout:
+                    _signOut();
+                    break;
+                }
+              },
+              itemBuilder: (BuildContext context) =>
+                  <PopupMenuEntry<MenuOptions>>[
+                    const PopupMenuItem<MenuOptions>(
+                      value: MenuOptions.impostazioniAccount,
+                      child: ListTile(
+                        leading: Icon(Icons.person),
+                        title: Text('Profilo'),
+                      ),
+                    ),
+                    const PopupMenuItem<MenuOptions>(
+                      value: MenuOptions.impostazioniApp,
+                      child: ListTile(
+                        leading: Icon(Icons.settings),
+                        title: Text('Impostazioni'),
+                      ),
+                    ),
+                    const PopupMenuItem<MenuOptions>(
+                      value: MenuOptions.logout,
+                      child: ListTile(
+                        leading: Icon(Icons.exit_to_app, color: Colors.red),
+                        title: Text('Esci', style: TextStyle(color: Colors.red)),
+                      ),
+                    ),
+                  ],
             ),
-          )
-        else
-          PopupMenuButton<MenuOptions>(
-            icon: const Icon(Icons.account_circle_outlined),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            position: PopupMenuPosition.under,
-            onSelected: (MenuOptions value) {
-              switch (value) {
-                case MenuOptions.impostazioniAccount:
-                  final String? currentRoute = ModalRoute.of(context)?.settings.name;
-                  if (currentRoute != Routes.profile) {
-                    Navigator.pushNamed(context, Routes.profile);
-                  }
-                  break;
-                case MenuOptions.impostazioniApp:
-                  _showAlert(context);
-                  break;
-                case MenuOptions.logout:
-                  _signOut();
-                  break;
-              }
-            },
-            itemBuilder: (BuildContext context) =>
-                <PopupMenuEntry<MenuOptions>>[
-                  const PopupMenuItem<MenuOptions>(
-                    value: MenuOptions.impostazioniAccount,
-                    child: ListTile(
-                      leading: Icon(Icons.person),
-                      title: Text('Profilo'),
-                    ),
-                  ),
-                  const PopupMenuItem<MenuOptions>(
-                    value: MenuOptions.impostazioniApp,
-                    child: ListTile(
-                      leading: Icon(Icons.settings),
-                      title: Text('Impostazioni'),
-                    ),
-                  ),
-                  const PopupMenuItem<MenuOptions>(
-                    value: MenuOptions.logout,
-                    child: ListTile(
-                      leading: Icon(Icons.exit_to_app, color: Colors.red),
-                      title: Text('Esci', style: TextStyle(color: Colors.red)),
-                    ),
-                  ),
-                ],
-          ),
+        ],
       ],
     );
   }
