3f7cd79436e46379b5eb8aa0400ed1bce32df246
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-06-16 18:27:15 +0200

    Aggiunte traduzioni per il blocco di ricarica dati.

--- lib/widgets/common/error_retry.dart
+++ lib/widgets/common/error_retry.dart
@@ -1,3 +1,4 @@
+import 'package:f1_news/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 
 class ErrorRetry extends StatelessWidget {
@@ -12,6 +13,8 @@ class ErrorRetry extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
+
     return Center(
       child: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -20,21 +23,16 @@ class ErrorRetry extends StatelessWidget {
           children: [
             const Icon(Icons.wifi_off_rounded, size: 48, color: Colors.grey),
             const SizedBox(height: 16),
-            const Text(
-              "Errore durante il caricamento dei dati! Riprova.",
+             Text(
+              l10n.errorRetry,
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 8),
-            // Text(
-            //   "Dettaglio: $errorMessage",
-            //   textAlign: TextAlign.center,
-            //   style: const TextStyle(fontSize: 12),
-            // ),
             const SizedBox(height: 24),
             ElevatedButton.icon(
               onPressed: onRetry,
               icon: const Icon(Icons.refresh),
-              label: const Text("Riprova"),
+              label: Text(l10n.retryButton),
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.red,
                 foregroundColor: Colors.white,
