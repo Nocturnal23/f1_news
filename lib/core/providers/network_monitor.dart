89b1aaf5e80da87d677782e9ed0517a7232a8316
Nocturnal23 <matarazzogennaro1997@gmail.com>
2026-06-17 20:06:04 +0200

    Bug fix

--- lib/core/providers/network_monitor.dart
+++ lib/core/providers/network_monitor.dart
@@ -6,46 +6,31 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'language_provider.dart';
 
-void showOfflineSnackBar(AppLocalizations l10n, bool isAppConnected) {
-  final messenger = scaffoldMessengerKey.currentState;
-  if (messenger == null) return;
-
-  messenger
-    ..clearSnackBars()
-    ..removeCurrentSnackBar();
-
-  messenger.showSnackBar(
-    SnackBar(
-      content: Text(
-        isAppConnected ?l10n.okConnection : l10n.noConnection,
-        style: const TextStyle(color: Colors.white),
-      ),
-      backgroundColor: isAppConnected ? Colors.green : Colors.red,
-      duration: const Duration(seconds: 5),
-    ),
-  );
-}
-
 final startNetworkMonitoring = Provider<void>((ref) {
-    InternetConnection().onStatusChange.listen((status) {
-      final connected = status == InternetStatus.connected;
+  bool isFirstEvent = true;
 
-      final currentLocale = ref.read(localeProvider);
-      final l10n = lookupAppLocalizations(currentLocale);
+  InternetConnection().onStatusChange.listen((status) {
+    final connected = status == InternetStatus.connected;
 
-      final messenger = scaffoldMessengerKey.currentState;
-      if (messenger == null) return;
+    final currentLocale = ref.read(localeProvider);
+    final l10n = lookupAppLocalizations(currentLocale);
 
-      messenger.clearSnackBars();
+    final messenger = scaffoldMessengerKey.currentState;
+    if (messenger == null) return;
 
-      messenger.showSnackBar(
-        SnackBar(
-          content: Text(
-            connected ? l10n.okConnection : l10n.noConnection,
-          ),
-          backgroundColor: connected ? Colors.green : Colors.red,
-          duration: const Duration(seconds: 5),
-        ),
-      );
-    });
+    if (isFirstEvent) {
+      isFirstEvent = false;
+      if (connected) return;
+    }
+
+    messenger.clearSnackBars();
+
+    messenger.showSnackBar(
+      SnackBar(
+        content: Text(connected ? l10n.okConnection : l10n.noConnection),
+        backgroundColor: connected ? Colors.green : Colors.red,
+        duration: const Duration(seconds: 5),
+      ),
+    );
+  });
 });
